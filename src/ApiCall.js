5b19cafc657be309562041600673c621de7d62d1
binhk1004 <binhk1004@naver.com>
2021-10-03 23:26:24 +0900

    [Components] Modified 'Test.js'

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 
-function ApiCall(){
+function ApiCall() {
+  const url = "/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+  const ServiceKey =
+    "jFJq%2B%2BJgU2Mc8PrE5BxRZetsGiBrM%2BXDSku%2FUFCuzZ7j8FrslWnJ%2BR2xa7QbRStVG9HfSDU%2BmBQz3SCSfZmfXw%3D%3D";
 
-    const url = '/openapi/service/rest/Covid19/getCovid19SidoInfStateJson';
-    const ServiceKey = 'jFJq%2B%2BJgU2Mc8PrE5BxRZetsGiBrM%2BXDSku%2FUFCuzZ7j8FrslWnJ%2BR2xa7QbRStVG9HfSDU%2BmBQz3SCSfZmfXw%3D%3D';
+  const result = axios.get(url + "?ServiceKey=" + ServiceKey);
 
-    const result = axios.get(url+'?ServiceKey='+ServiceKey);
-    
-        function getResult(){
-            return new Promise((resolve, reject)=>{
-                if (result){
-                    resolve(result)
-                }
-                reject("응답이 거절 되었습니다.")
-            })
-        };
-    
-        getResult().then(function(showData){
-            const test = showData.data.response.body.items.item[1].gubun
-            console.log(test)
-        })
+  function getResult() {
+    return new Promise((resolve, reject) => {
+      if (result) {
+        resolve(result);
+      }
+      reject("응답이 거절 되었습니다.");
+    });
+  }
 
-    return(
-        <div></div>
-    )
+  getResult().then(function (showData) {
+    const test = showData.data.response.body.items.item[1].gubun;
+    console.log(test);
+  });
+
+  return <div></div>;
 }
 
 export default ApiCall;
--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,18 @@
-import test from './ApiCall'
+import { useState } from "react";
+import axios from "axios";
 
-const ResultSet = () => {
+const Test = () => {
+
+    const [reqData, resDate] = useState('');
+
+    const url = "/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+    const ServiceKey ="jFJq%2B%2BJgU2Mc8PrE5BxRZetsGiBrM%2BXDSku%2FUFCuzZ7j8FrslWnJ%2BR2xa7QbRStVG9HfSDU%2BmBQz3SCSfZmfXw%3D%3D";
+
+    axios.get(url + "?ServiceKey=" + ServiceKey).then(Response => {reqData(Response.data)})
+
     return(
-        <div>
-            {test.map((test)=><div>{test}</div>)}
-        </div>
+        <div>{}</div>
     )
 }
 
-export default ResultSet;
+export default Test;
