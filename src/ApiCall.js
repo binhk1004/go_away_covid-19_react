467d4b1a90e66277d96b67351610522093d46d2d
binhk1004 <binhk1004@naver.com>
2021-10-11 22:04:45 +0900

    [Components] Modified Components 'Map.js' and 'ApiCall.js'

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -5,13 +5,12 @@ import { useEffect, useState } from "react";
 function ApiCall() {
   const [data, setData] = useState('');
   const url = "/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
-  const ServiceKey =
-    "jFJq%2B%2BJgU2Mc8PrE5BxRZetsGiBrM%2BXDSku%2FUFCuzZ7j8FrslWnJ%2BR2xa7QbRStVG9HfSDU%2BmBQz3SCSfZmfXw%3D%3D";
+  const ServiceKey = "jFJq%2B%2BJgU2Mc8PrE5BxRZetsGiBrM%2BXDSku%2FUFCuzZ7j8FrslWnJ%2BR2xa7QbRStVG9HfSDU%2BmBQz3SCSfZmfXw%3D%3D";
 
   const result = axios.get(url + "?ServiceKey=" + ServiceKey);
 
   useEffect(() => {
-    async function getResult() {
+    function getResult() {
       return new Promise((resolve, reject) => {
         if (result) {
           resolve(result);
@@ -20,22 +19,27 @@ function ApiCall() {
       });
     }
 
+    
      getResult().then(function (showData) {
+
       const requestDate = showData.data.response.body.items.item;
 
       const gubunData = [];
       const incDecData = [];
+      const totalData = []
       const finalData = {};
 
       for (let i = 0; i < requestDate.length; i++) {
         gubunData.push(requestDate[i].gubun);
         incDecData.push(requestDate[i].incDec);
+        totalData.push(requestDate[i].defCnt)
       }
       finalData["gubun"] = gubunData;
       finalData["incDec"] = incDecData; 
+      finalData["total"] = totalData;
       setData(finalData);
-    });
-    getResult();
+    })
+      getResult();
   },[]);
 
   const covid = data;
