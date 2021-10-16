292417ae1ac687c3a06a53e3e8b5f17a9bde6d65
binhk1004 <binhk1004@naver.com>
2021-10-16 23:19:43 +0900

    [CSS] Modified 'Map.css' and 'ResultTable.css'

--- src/ResultTable.js
+++ src/ResultTable.js
@@ -1,29 +1,19 @@
 import "./ResultTable.css";
 
-export default function ResultTable(testData) {
-
-    const gubunData = testData.apiresult.apiresult.gubun; //행정구역 구분
-    const incDecData = testData.apiresult.apiresult.incDec; //전일대비 증감수
-    const totalData = testData.apiresult.apiresult.total; //확진자 수
-
-    if (gubunData !== "undefined" && gubunData !== null){}
+export default function ResultTable() {
 
   return (
     <table>
-      <thead>
         <tr>
           <td>지역</td>
           <td>전일대비 증가수</td>
           <td>누적 확진자 수</td>
         </tr>
-      </thead>
-      <tbody>
         <tr>
-          <td>{gubunData[4]}</td>
-          <td>{incDecData[4]}</td>
-          <td>{totalData[4]}</td>
+          <td>테스트</td>
+          <td>테스트</td>
+          <td>테스트</td>
         </tr>
-      </tbody>
     </table>
   );
 }
