fd93d7527f1738fdfba8d951cf2ce2fa6f88251a
binhk1004 <binhk1004@naver.com>
2021-11-26 10:49:18 +0900

    [Components] Modified 'ApiCall.js'

--- src/ApiCall.js
+++ src/ApiCall.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 function ApiCall() {
   const [data, setData] = useState('');
-  const url = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+  const url = "getCovid19SidoInfStateJson";
   const ServiceKey = "jFJq%2B%2BJgU2Mc8PrE5BxRZetsGiBrM%2BXDSku%2FUFCuzZ7j8FrslWnJ%2BR2xa7QbRStVG9HfSDU%2BmBQz3SCSfZmfXw%3D%3D";
 
   const result = axios.get(url + "?ServiceKey=" + ServiceKey);
